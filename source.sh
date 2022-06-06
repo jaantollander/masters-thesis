69689eeee7bfce4783ef962635373f34b81d4780
Jaan Tollander de Balsch <jaan@hey.com>
2022-06-06 08:40:57 +0300

    initial commit

---
+++ source.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+IN_DIR=content
+OUT_DIR=build
+
+html() {
+    mkdir -p "$OUT_DIR"
+    pandoc "$IN_DIR/index.md" \
+        --citeproc \
+        --standalone \
+        --from "markdown+tex_math_dollars" \
+        --to "html" \
+        --output "$OUT_DIR/index.html" \
+        --metadata "pagetitle=Master's Thesis"
+}
+
+pdf() {
+    mkdir -p "$OUT_DIR"
+    pandoc "$IN_DIR/index.md" \
+        --citeproc \
+        --from "markdown+tex_math_dollars+raw_tex" \
+        --to "latex" \
+        --output "$OUT_DIR/index.pdf" \
+        --pdf-engine="xelatex"
+}
