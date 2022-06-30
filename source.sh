d887dee08889ec4458e1b1d9712a3a1ae20d1f3b
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2022-06-30 09:50:59 +0300

    renamed data to assets, fixed thesis_preview

--- source.sh
+++ source.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 IN_DIR=$PWD/content
 OUT_DIR=$PWD/build
-DATA_DIR=$PWD/data
+ASSETS_DIR=$PWD/assets
 
-export TEXINPUTS="::$DATA_DIR"
+export TEXINPUTS="::$ASSETS_DIR"
 
 thesis_download_aaltostyle() {
-    curl --location "https://wiki.aalto.fi/download/attachments/69900685/aaltothesis.cls?api=v2" --output "aaltothesis.cls"
+    curl --location "https://wiki.aalto.fi/download/attachments/69900685/aaltothesis.cls?api=v2" --output "$ASSETS_DIR/aaltothesis.cls"
     TMP="$(mktemp -d)"
     curl --location "https://wiki.aalto.fi/download/attachments/49383512/aaltologo.zip" --output "$TMP/aaltologo.zip"
     unzip "$TMP/aaltologo.zip" -d "$TMP"
-    mv "$TMP/aaltologo.sty" "."
+    mv "$TMP/aaltologo.sty" "$ASSETS_DIR"
+}
+
+thesis_download_citationstyle() {
+    curl --location "https://raw.githubusercontent.com/citation-style-language/styles/master/harvard-anglia-ruskin-university.csl" --output "$ASSETS_DIR/citationstyle.csl"
 }
 
 thesis_html() {
@@ -21,11 +25,12 @@ thesis_html() {
         --from "markdown+tex_math_dollars" \
         --to "html" \
         --output "$OUT_DIR/index.html" \
-        --katex \
+        --mathjax \
         --metadata "pagetitle=Master's Thesis" \
         --metadata "date=$(date -I)" \
-        --metadata-file "$DATA_DIR/metadata.yaml" \
-        --bibliography "$DATA_DIR/bibliography.bib"
+        --metadata-file "$ASSETS_DIR/metadata.yaml" \
+        --bibliography "$ASSETS_DIR/bibliography.bib" \
+        --csl "$ASSETS_DIR/citationstyle.csl"
 }
 
 thesis_pdf() {
@@ -37,9 +42,10 @@ thesis_pdf() {
         --output "$OUT_DIR/index.pdf" \
         --pdf-engine="pdflatex" \
         --metadata "date=$(date -I)" \
-        --metadata-file "$DATA_DIR/metadata.yaml" \
-        --bibliography "$DATA_DIR/bibliography.bib" \
-        --include-in-header "$DATA_DIR/header.tex"
+        --metadata-file "$ASSETS_DIR/metadata.yaml" \
+        --bibliography "$ASSETS_DIR/bibliography.bib" \
+        --csl "$ASSETS_DIR/citationstyle.csl" \
+        --include-in-header "$ASSETS_DIR/header.tex"
 }
 
 thesis_tex() {
@@ -50,19 +56,21 @@ thesis_tex() {
         --to "latex" \
         --output "$OUT_DIR/index.tex" \
         --metadata "date=$(date -I)" \
-        --metadata-file "$DATA_DIR/metadata.yaml" \
-        --bibliography "$DATA_DIR/bibliography.bib" \
-        --include-in-header "$DATA_DIR/header.tex"
+        --metadata-file "$ASSETS_DIR/metadata.yaml" \
+        --bibliography "$ASSETS_DIR/bibliography.bib" \
+        --csl "$ASSETS_DIR/citationstyle.csl" \
+        --include-in-header "$ASSETS_DIR/header.tex"
 }
 
 thesis_preview() {
-    html
+    CMD="thesis_html"
+    $CMD
     # Run html command if files in target directories change.
     # https://superuser.com/questions/181517/how-to-execute-a-command-whenever-a-file-changes
-    inotifywait -e close_write,moved_to,create -m $IN_DIR -m $DATA_DIR |
+    inotifywait -e close_write,moved_to,create -m "$IN_DIR" -m "$ASSETS_DIR" |
     while read -r directory events filename; do
         case $filename in 
-            *.md|*.yaml|*.bib) html ;;
+            *.md|*.bib) $CMD ;;
             *) ;;
         esac
     done
