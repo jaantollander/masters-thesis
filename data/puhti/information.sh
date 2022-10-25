b9b4290a89087d588235e380319f6c2576d6542a
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2022-10-25 11:26:39 +0300

    data

--- data/puhti/information.sh
+++ data/puhti/information.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
-cat /etc/redhat-release > versions.txt
-lctl lustre_build_version >> versions.txt
-sinfo --version >> versions.txt
+cat /proc/version > information.txt
+cat /etc/redhat-release >> information.txt
+lctl lustre_build_version >> information.txt
+sinfo --version >> information.txt
 scontrol show partitions --all > slurm-partitions.txt
 scontrol show node --all > compute-nodes.txt
 grep 'NodeName=' compute-nodes.txt | cut -c 10-15 | sort > compute-nodenames.csv
+lctl get_param jobid_var jobid_name > information.txt
